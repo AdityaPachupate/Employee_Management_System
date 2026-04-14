f2d54723b8b9a8fdb202611a55aa2393335ca967
Riyaj Shaikh <riyaj.sshaikh88@gmail.com>
2026-04-14 18:05:57 +0530

    Add .gitignore file
    Add .gitignore file

--- EmployeeService/obj/Debug/net8.0/EmployeeService.AssemblyInfo.cs
+++ EmployeeService/obj/Debug/net8.0/EmployeeService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a063a7c4d83b82feec4e16857d184d9ac9a31392")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
