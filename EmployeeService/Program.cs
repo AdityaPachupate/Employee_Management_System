c48129b91f73f1f5675b5818597ab47718380a6c
Aditya Pachupate <adityapachupate@gmail.com>
2026-04-22 21:15:05 +0530

    feat: implement in-memory StatsDB update mechanism in EmployeeService

--- EmployeeService/Program.cs
+++ EmployeeService/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.OpenApi.Models;
+using DepartmentService.Infrastructure.Persistence;
+using DepartmentService.Infrastructure.Messaging;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,11 +18,18 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<EmployeeDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
 
+builder.Services.AddDbContext<DepartmentDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
 
 builder.Services.AddMassTransit(x =>
 {
+    x.AddConsumer<EmployeeEventConsumer>();
 
     x.UsingInMemory((context, cfg) =>
+
     {
         cfg.ConfigureEndpoints(context);
     });
